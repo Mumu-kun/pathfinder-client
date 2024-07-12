935516e66c624e5ade078a8dadf0a74c39517aa2
mehedikhan72 <lopingcard@gmail.com>
2024-07-12 23:55:44 +0600

    class rename

--- src/pages/Home/Intro.tsx
+++ src/pages/Home/Intro.tsx
@@ -4,7 +4,7 @@ import ThemeBtn from "../../components/ChangeTheme";
 const Intro: React.FC = () => {
 	return (
 		<div className="m-20">
-			<div className="flex items-center justify-between rounded-lg secondary-color">
+			<div className="flex items-center justify-between rounded-lg secondary-color-bg">
 				<div className="w-1/2">
 					<p className="large-headings text-left rounded-l-lg p-20">
 						Find yourself the perfect mentor based on your interests and goals.
