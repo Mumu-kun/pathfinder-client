8a5de0dcfb51932e6e8085f9c413225af53d8533
Mustafa Muhaimin <mumufahad@gmail.com>
2024-09-25 18:41:02 +0600

    fix

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -12,7 +12,7 @@ const Tag = ({ tag, disabled = false, className: pClassName }: TagProps) => {
 	const [isDisabled, setIsDisabled] = useState<boolean>(disabled);
 	return (
 		<div
-			className={`mr-2 inline-block w-max rounded-sm border-2 border-solid border-zinc-400 bg-white px-1 text-xss font-medium leading-relaxed text-zinc-600 dark:bg-dark-secondary dark:font-bold dark:text-zinc-200 ${!isDisabled ? "cursor-pointer hover:border-solid" : "cursor-default"} ${pClassName}`}
+			className={`mr-2 inline-block w-max whitespace-nowrap rounded-sm border-2 border-solid border-zinc-400 bg-white px-1 text-xss font-medium leading-relaxed text-zinc-600 dark:bg-dark-secondary dark:font-bold dark:text-zinc-200 ${!isDisabled ? "cursor-pointer hover:border-solid" : "cursor-default"} ${pClassName ?? ""}`}
 			onMouseEnter={(e) => {
 				e.altKey && setIsDisabled(true);
 			}}
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -34,6 +34,7 @@ const Header = () => {
 	const [stuck, setStuck] = useState<boolean>(!isHome || window.scrollY > 0);
 
 	const isMediaMD = useMediaQuery("(max-width: 768px)");
+	const isMediaLG = useMediaQuery("(max-width: 1024px)");
 
 	useEffect(() => {
 		isHome ? setStuck(Boolean(window.scrollY)) : setStuck(true);
@@ -58,8 +59,13 @@ const Header = () => {
 				>
 					{/* Logo */}
 					<div className="col-span-1 flex items-center gap-2 px-2">
-						<Link to="/" className="mr-6 h-12">
+						<Link to="/" className="mr-6 flex h-12 items-end gap-1">
 							<img src={Logo} alt="pathPhindr" className="h-full max-w-full object-contain pt-3" />
+							<div
+								className={`font-prim pb-2.5 text-[1.7rem] font-bold ${!isMediaLG && stuck ? "opacity-0" : ""} transition-all`}
+							>
+								ath<span className="text-green-500">Phindr</span>
+							</div>
 						</Link>
 					</div>
 
